125de6b171ecd005a3c786db16215fd7badb34af
Garba Fuad <garba@impactbytes.io>
2023-03-09 14:46:58 +0100

    Define Models and add Tables

--- GameReview/api/models.py
+++ GameReview/api/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 # Create your models here.
+
+class User(models.Model):
+    username = models.CharField(max_length=20, unique=True)
+    email = models.EmailField()
+
+    def __str__(self) -> str:
+        return self.username
+
+class VideoGame(models.Model):
+    name= models.CharField(max_length=120)
+    image = models.ImageField(upload_to='uploads/')
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Review(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    video_game = models.ForeignKey(VideoGame, on_delete=models.CASCADE )
+    star_rating = models.IntegerField(default=0, validators=[
+        MaxValueValidator(5), MinValueValidator(0)
+    ])
+    comment = models.TextField()
+    location = models.CharField(max_length=100)
+
+
+    def __str__(self) -> str:
+        return self.user
